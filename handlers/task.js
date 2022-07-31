c28343d498a96bd80a2e84a2aaaebfdcb47a8486
Dimas Ananda <traperwaze@gmail.com>
2022-07-31 19:58:29 +0700

    return tasks as array directly, not object with key "tasks"
    Signed-off-by: Dimas Ananda <traperwaze@gmail.com>

--- handlers/task.js
+++ handlers/task.js
@@ -4,7 +4,7 @@ module.exports = {
     async index(req) {
         const tasks = await prisma.task.findMany();
 
-        return { tasks };
+        return tasks;
     },
     async show(req) {},
     async store(req) {},
